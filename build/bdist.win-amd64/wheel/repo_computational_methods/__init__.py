b6f58f367225701edb857a776179c0ebecf8e985
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2026-04-17 15:04:38 -0500

    udpate all repo

---
+++ build/bdist.win-amd64/wheel/repo_computational_methods/__init__.py
@@ -0,0 +1,6 @@
+from .core_simple_class_MC import DistStress_Force_Mom_Frame_CofSimp, Plot_Distribution_Stress_Force_Moments
+
+__all__ = [
+    "DistStress_Force_Mom_Frame_CofSimp",
+    "Plot_Distribution_Stress_Force_Moments"
+]
