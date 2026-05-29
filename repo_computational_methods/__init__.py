4110d944cf17f5c53cc258bfe5c28fc1f7654c5a
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2026-05-29 08:43:18 -0500

    update repo

--- repo_computational_methods/__init__.py
+++ repo_computational_methods/__init__.py
@@ -2,11 +2,13 @@ from .core_simple_class_MC import (DistStress_Force_Mom_Frame_CofSimp,
                                    Plot_Distribution_Stress_Force_Moments,
                                    ReadExcelFies_compute_avarage_stress,
                                    SPEC_NEC_2024,
+                                   DisForceLat_NEC_2024
                                    )
 
 __all__ = [
     "DistStress_Force_Mom_Frame_CofSimp",
     "Plot_Distribution_Stress_Force_Moments",
     "ReadExcelFies_compute_avarage_stress",
-    "SPEC_NEC_2024"
+    "SPEC_NEC_2024",
+    "DisForceLat_NEC_2024"
 ]
